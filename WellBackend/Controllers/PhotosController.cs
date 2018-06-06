3fa7b0ee1f0976a396f3b6cd746caf25f53162ab
colladocaballero <ccolladocaballero@gmail.com>
2018-06-06 23:22:25 +0200

    Photos: upload and get by user id

---
+++ WellBackend/Controllers/PhotosController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WellBackend.Models;
+using WellBackend.Services;
+
+namespace WellBackend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Photos")]
+    public class PhotosController : Controller
+    {
+        private readonly IPhotosService _photosService;
+
+        public PhotosController(IPhotosService photosService)
+        {
+            _photosService = photosService;
+        }
+
+        // GET: api/Photos/mail@mail.com
+        [HttpGet("{userId}", Name = "GetByUserId")]
+        public IActionResult GetByUserId(string userId)
+        {
+            return new OkObjectResult(new
+            {
+                statusCode = 200,
+                data = _photosService.GetByUserId(userId)
+            });
+        }
+        
+        // POST: api/Photos
+        [HttpPost]
+        public async Task<IActionResult> UploadImage([FromForm]IFormFile image, [FromForm]string userId)
+        {
+            return Created("ImageUploaded", new
+            {
+                statusCode = 201,
+                data = await _photosService.UploadImage(image, userId)
+            });
+        }
+        
+        // PUT: api/Photos/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WellBackend/Services/PhotosService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using WellBackend.Contexts;
+using WellBackend.Models;
+
+namespace WellBackend.Services
+{
+    public class PhotosService : IPhotosService
+    {
+        private readonly WellDbContext _wellDbContext;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public PhotosService(WellDbContext wellDbContext, IHostingEnvironment hostingEnvironment)
+        {
+            _wellDbContext = wellDbContext;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IEnumerable<Photo> GetByUserId(string userId)
+        {
+            return _wellDbContext.Photos.Where(photo => photo.UserId == userId);
+        }
+
+        public async Task<string> UploadImage(IFormFile image, string userId)
+        {
+            string imageName = Path.GetFileNameWithoutExtension(image.FileName); // Image's name withour extension
+            string imageExtension = Path.GetExtension(image.FileName); // Image's extension
+
+            Regex datePattern = new Regex("[/: ]"); // RegEx for date formatting
+            string uploadTime = datePattern.Replace(DateTime.Now.ToString(), ""); // Date and time of the upload
+
+            string fileName = imageName + uploadTime + imageExtension;
+
+            DirectoryInfo di = Directory.CreateDirectory(_hostingEnvironment.WebRootPath + "\\img\\" + userId);
+
+            using (FileStream fs = File.Create(_hostingEnvironment.WebRootPath + "\\img\\" + userId + "\\" + fileName))
+            {
+                image.CopyTo(fs);
+                fs.Flush();
+            }
+
+            Photo photo = new Photo
+            {
+                FileName = fileName,
+                UploadDate = DateTime.Now,
+                Likes = 0,
+                UserId = userId,
+                User = _wellDbContext.UsersWell.FirstOrDefault(user => user.Id == userId)
+            };
+
+            await _wellDbContext.Photos.AddAsync(photo);
+
+            await _wellDbContext.SaveChangesAsync();
+
+            return fileName;
+        }
+    }
+}
